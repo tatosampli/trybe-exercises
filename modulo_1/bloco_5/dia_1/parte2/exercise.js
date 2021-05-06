61e266d2d886cc76180a4498f2ee445cb4739e6b
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-05-06 12:55:10 -0300

    Começa exercício principal do dia 1.

---
+++ modulo_1/bloco_5/dia_1/parte2/exercise.js
@@ -0,0 +1,10 @@
+var paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.getElementById('page-title').innerHTML = 'Resident Evil';
+document.getElementById('second-paragraph').innerHTML = 'S.T.A.R.S!';
+document.getElementById('subtitle').innerHTML = "Alice's movie";
+
+document.getElementsByClassName('paragraph2')[0].style.color = 'blue';
+
+document.getElementsByTagName('h4')[0].style.color = 'red';
