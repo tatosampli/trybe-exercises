4635c653bb7198de836094e8d0a5459d1cb91665
Tharcio Sampaio <tatosampaiolima@gmail.com>
2021-05-14 14:43:28 -0300

    Termina exercício bloco 6 - dia 1.

--- modulo_1/bloco_6/dia_1/exercises/script.js
+++ modulo_1/bloco_6/dia_1/exercises/script.js
@@ -1,5 +1,9 @@
 const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás, Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+const formsId = ['input-name', 'input-email', 'input-cpf', 'input-address', 'input-city', 'input-state', 'type', 'input-summary', 'input-job', 'input-description', 'input-date'];
 const registerButton = document.getElementById('registerButton');
+const eraseButton = document.getElementById('eraseButton');
+const main = document.getElementById('main');
+let originalTypeIDList = [];
 
 function createStates(states) {
   const selectStates = document.getElementById('input-state');
@@ -62,8 +66,46 @@ function dateInput() {
   return (responseDayIsValid && responseMonthIsValid && responseYearIsValid);
 }
 
+function typeListId() {
+  const typeList = document.querySelectorAll('.typeClass');
+  for (let index = 0; index < typeList.length; index += 1) {
+    originalTypeIDList.push(typeList[index].id);
+  }
+}
+
+function typeChanger() {
+  const typeList = document.querySelectorAll('.typeClass');
+  for (let index = 0; index < typeList.length; index += 1) {
+    if (typeList[index].checked) {
+      typeList[index].id = 'type';
+    }
+    if (!typeList[index].checked) {
+      typeList[index].id = originalTypeIDList[index];
+    }
+  }
+}
+
+function getForm(formsId) {
+  let answeredForm = document.getElementById('complete-form');
+  let elementDiv = document.createElement('div');
+  elementDiv.id = 'resultDiv';
+  main.appendChild(elementDiv);
+  answeredForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    for (let index = 0; index < formsId.length; index += 1) {
+      let elementP = document.createElement('p');
+      elementDiv.appendChild(elementP);
+      let elementId = document.getElementById(`${formsId[index]}`);
+      elementP.innerText = `${elementId.name}: ${elementId.value}`;
+    }
+    let quebraLinha = document.createElement('br');
+    elementDiv.appendChild(quebraLinha);
+  });
+}
+
 function submitButton() {
   registerButton.addEventListener('click', (event) => {
+    typeChanger();
     const dateInputIsValid = dateInput();
     if (!dateInputIsValid) {
       event.preventDefault();
@@ -71,5 +113,23 @@ function submitButton() {
   });
 }
 
+function clearButton() {
+  eraseButton.addEventListener('click', () => {
+    const elementDiv = document.getElementById('resultDiv');
+    const inputAll = document.getElementsByTagName('input');
+    const textAreaAll = document.getElementsByTagName('textarea');
+    main.removeChild(elementDiv);
+    for (let index = 0; index < inputAll.length; index += 1) {
+      inputAll[index].value = '';
+    }
+    for (let index = 0; index < textAreaAll.length; index += 1) {
+      textAreaAll[index].value = '';
+    }
+  });
+}
+
 createStates(states);
+typeListId();
+getForm(formsId);
 submitButton();
+clearButton();
